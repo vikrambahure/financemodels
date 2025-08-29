6edd115cffcfd1ebca9c99a5a229553fb017e691
unknown <economicsvikram@gmail.com>
2025-08-29 17:12:51 +0100

    v0.1.1: new models and changing init

--- src/equitymodels/models/__init__.py
+++ src/equitymodels/models/__init__.py
@@ -1,1 +1,7 @@
-# empty file
+from .macro_fama import MacroConfig, run_analysis_with_macro
+from .kalman_fama import KalmanConfig, run_analysis_with_kalman
+
+__all__ = [
+    "MacroConfig", "run_analysis_with_macro",
+    "KalmanConfig", "run_analysis_with_kalman",
+]
