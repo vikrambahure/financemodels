b9ed69c1859d46aced3e9796179b616f81553a8f
unknown <economicsvikram@gmail.com>
2025-08-29 21:25:11 +0100

    Added init py models

--- src/equitymodels/models/__init__.py
+++ src/equitymodels/models/__init__.py
@@ -1,7 +1,9 @@
 from .macro_fama import MacroConfig, run_analysis_with_macro
-from .kalman_fama import(
-    tvp_plot_regimes,
-    tvp_label_regimes
-)
+from .kalman_fama import tvp_plot_regimes, tvp_label_regimes
+
 __all__ = [
-    "MacroConfig", "run_analysis_with_macro",
+    "MacroConfig",
+    "run_analysis_with_macro",
+    "tvp_plot_regimes",
+    "tvp_label_regimes",
+]
